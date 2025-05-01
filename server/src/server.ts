35599dc7ce3cb4f1692b2d47e417c66b6bcd155f
zachmc27 <dzdudes1213@gmail.com>
2025-05-01 17:51:59 -0500

    adjusted server.ts

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,5 +1,5 @@
 import express from 'express';
-import path from 'node:path';
+
 import db from './config/connection.js';
 // import routes from './routes/index.js';
 import type { Request, Response } from 'express';
@@ -7,13 +7,6 @@ import { ApolloServer } from '@apollo/server';
 import { expressMiddleware } from '@apollo/server/express4';
 import { authenticateToken } from './services/auth.js';
 import { typeDefs, resolvers } from './schemas/index.js';
-
-import { fileURLToPath } from 'url';
-
-
-// Simulate __dirname in ES modules
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
 
 
 const server = new ApolloServer({
@@ -40,10 +33,10 @@ const startApolloServer = async () => {
   ));
 
   if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/dist')));
+    app.use(express.static('../client/dist'));
 
     app.get('*', (_req: Request, res: Response) => {
-      res.sendFile(path.join(__dirname, '../client/dist/index.html'));
+      res.sendFile('../client/dist/index.html');
     });
   }
 
